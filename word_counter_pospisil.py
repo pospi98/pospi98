02f1a05a20ce6d2ef55502e01f559029039d62ff
pospi98 <60886628+pospi98@users.noreply.github.com>
2020-04-29 19:02:55 +0200

    Add files via upload

--- word_counter_pospisil.py
+++ word_counter_pospisil.py
@@ -1,47 +1,38 @@
 import argparse
 import sys
-
+import requests
 
 def main():
     newparser = argparse.ArgumentParser(description='Soubor.')
-    newparser.add_argument('soubor')
+    newparser.add_argument("website", help="název webstranky")
     newparser.add_argument("znaky", help="Spočítat znaky", action="store_true")
     newparser.add_argument("slova", help="Spočítat slova", action="store_true")
     newparser.add_argument("radky", help="Spočítat řádky", action="store_true")
     arguments = newparser.parse_args()
     try:
-        f = open(arguments.soubor, "r")
-        soubor = f.read()
+        r = requests.get(arguments.website)
+        f = r.text
         flag = False
         if arguments.slova:
-            pocet_slov = word_counter(soubor)
+            pocet_slov = f.count(' ') + len(f.split('\n'))
             print(f"Počet slov: {pocet_slov}")
             flag = True
         if arguments.znaky:
-            pocet_znaku = znaky_counter(soubor)
+            pocet_znaku = len(f)
             print(f"Počet znaků: {pocet_znaku}")
             flag = True
         if arguments.radky:
-            pocet_radku = line_counter(soubor)
+            pocet_radku = len(f.split('\n'))
             print(f"Počet řádků: {pocet_radku}")
             flag = True
         if not flag:
-            pocet_slov = word_counter(soubor)
-            pocet_znaku = znaky_counter(soubor)
-            pocet_radku = line_counter(soubor)
+            pocet_slov = f.count(' ') + len(f.split('\n'))
+            pocet_znaku = len(f)
+            pocet_radku = len(f.split('\n'))
             print(f"Počet slov: {pocet_slov}\nPočet znaků: {pocet_znaku}\nPočet řádků: {pocet_radku}")
     except:
         print("Something went wrong")
         sys.exit(1)
-def word_counter(f):
-    pocet_slov = f.count(' ') + len(f.split('\n'))
-    return pocet_slov
-    pass
-def line_counter(f):
-    pocet_radku = len(f.split('\n'))
-    return pocet_radku
-def znaky_counter(f):
-    pocet_znaku = len(f)
-    return pocet_znaku
+
 
 main()
